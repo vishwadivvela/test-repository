de30beca4172e78086b941d29b2b2a047104f5f6
Cshanmukh98 <chintada.shanmukha955@gmail.com>
2024-10-01 22:56:15 +0530

    updated main class

--- src/main/java/com/test/project/demo/DemoApplication.java
+++ src/main/java/com/test/project/demo/DemoApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-
+ 
+	// main method
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
