aed03b26a551f4ea7f7ca60aa2dddc54df74c95c
Cshanmukh98 <chintada.shanmukha955@gmail.com>
2024-10-01 23:53:27 +0530

    'changes'

--- src/main/java/com/test/project/demo/DemoApplication.java
+++ src/main/java/com/test/project/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
  
-	// java application latest code
+	// java application latest code running
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
