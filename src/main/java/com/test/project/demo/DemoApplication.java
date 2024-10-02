ae10ef81172ef0c7a5afcea0a899a2a92d434709
Cshanmukh98 <chintada.shanmukha955@gmail.com>
2024-10-02 11:22:07 +0530

    message

--- src/main/java/com/test/project/demo/DemoApplication.java
+++ src/main/java/com/test/project/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
  
-	// main method
+	// application running successfully
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
