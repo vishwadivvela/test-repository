d863882331edf93fa5796adce4f2b73ff184a34c
Cshanmukh98 <chintada.shanmukha955@gmail.com>
2024-10-02 11:12:21 +0530

    new feature\

--- src/main/java/com/test/project/demo/DemoApplication.java
+++ src/main/java/com/test/project/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
  
 	
 	public static void main(String[] args) {
-		// run application
+		// run application successfully
 		SpringApplication.run(DemoApplication.class, args);
 		
 	}
