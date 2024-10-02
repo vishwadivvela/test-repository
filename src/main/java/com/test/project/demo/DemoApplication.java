89835bd731aafd754bdda155048cb5c7b876ef73
Cshanmukh98 <chintada.shanmukha955@gmail.com>
2024-10-02 15:16:56 +0530

    message added about main method

--- src/main/java/com/test/project/demo/DemoApplication.java
+++ src/main/java/com/test/project/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
  
-	// main method
+	// without main method code can be compiled but cannot be executed
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
